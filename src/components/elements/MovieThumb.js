b44da627103dff18c2c7b76a4bf77fe39070139c
Bgstatic <bilgehangecici@gmail.com>
2020-08-29 14:55:06 +0300

    Styling for MovieThumb.js.

--- src/components/elements/MovieThumb.js
+++ src/components/elements/MovieThumb.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import {StyledMovieThumb} from '../styles/StyledMovieThumb';
 
-const MovieThumb = () => {
-    return (
-        <div>
-            Movie
-        </div>
-    );
-}
+const MovieThumb = ({image, movieId, clickable}) => (
+
+    <StyledMovieThumb>
+      {
+          clickable ? (
+              <img className="clickable" src={image} alt="moviethumb" />
+          ) : (
+              <img src={image} alt="moviethumb"/>
+          )
+      }
+
+    </StyledMovieThumb>
+)
+    
+
 
 export default MovieThumb;
 
