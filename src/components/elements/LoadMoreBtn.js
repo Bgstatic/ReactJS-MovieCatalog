2640fcc348f97fac667ee5c7ffde6b80b81bd5eb
Bgstatic <bilgehangecici@gmail.com>
2020-08-30 13:46:36 +0300

    Load More Button styling and integration.

--- src/components/elements/LoadMoreBtn.js
+++ src/components/elements/LoadMoreBtn.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
-const LoadMoreBtn = () => {
-    return (
-        <div>
-            Load
-        </div>
-    );
-}
+import { StyledLoadMoreBtn } from '../styles/StyledLoadMoreBtn';
+
+const LoadMoreBtn = ({ text, callback }) => (
+
+    <StyledLoadMoreBtn type="button" onClick={callback}>
+        {text}
+    </StyledLoadMoreBtn>
+
+)
+
 
 export default LoadMoreBtn;
 
