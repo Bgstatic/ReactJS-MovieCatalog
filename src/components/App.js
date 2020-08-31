b02875ef399e408d5b0f5ed0d3bc0a886535ab38
Bgstatic <bilgehangecici@gmail.com>
2020-08-31 14:33:07 +0300

    Router integration.

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { Router } from '@reach/router';
 import Header from './elements/Header';
 import Home from './Home';
+import Movie from './Movie';
+import NotFound from './NotFound';
 
 import { GlobalStyle } from './styles/GlobalStyle';
 
@@ -9,7 +12,11 @@ const App = () => (
 
     <React.Fragment>
         <Header />
-        <Home />
+        <Router>
+            <Home path="/"/>
+            <Movie path="/:movieId"/>
+            <NotFound default />
+        </Router>
         <GlobalStyle />
     </React.Fragment>
 
