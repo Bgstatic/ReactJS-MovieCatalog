1350cd7a2221b95fb7e94d5c1bdcfeac0891dc3d
Bgstatic <bilgehangecici@gmail.com>
2020-08-31 14:48:56 +0300

    Movie page skeleton done.

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import Navigation from './elements/Navigation';
+import MovieInfo from './elements/MovieInfo';
+import MovieInfoBar from './elements/MovieInfoBar'
+import Actor from './elements/Actor';
+import Grid from './elements/Grid';
+import spinner from './elements/Spinner';
+import Spinner from './elements/Spinner';
 
 const Movie = ({ movieId }) => {
     return (
-        <div>
-            Movie: {movieId}
-        </div>
+        <>
+            <Navigation />
+            <MovieInfo />
+            <MovieInfoBar />
+            <Grid>
+                <Actor />
+            </Grid>
+            <Spinner />
+        </>
     );
 }
 
