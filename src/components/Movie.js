d32f96405dd355a3fb13d4cf46b5b22c780ae980
Bgstatic <bilgehangecici@gmail.com>
2020-09-01 14:06:09 +0300

    Updated spinner for Home.js.

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -11,17 +11,19 @@ import { useMovieFetch } from './hooks/useMovieFetch';
 
 const Movie = ({ movieId }) => {
 
-    const[movie, loading, error] = useMovieFetch(movieId);
+    const [movie, loading, error] = useMovieFetch(movieId);
+
+    if (error) return <div>Something went wrong...</div>
+    if (loading || !movie.original_title) return <Spinner />
 
     return (
         <>
-            <Navigation />
-            <MovieInfo />
+            <Navigation movie={movie.original_title} />
+            <MovieInfo movie={movie} />
             <MovieInfoBar />
             <Grid>
                 <Actor />
             </Grid>
-            <Spinner />
         </>
     );
 }
